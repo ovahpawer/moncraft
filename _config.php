3075054fe0703d7b9dc3555554d9c74e3e20a884
ovahpawer <95159136+ovahpawer@users.noreply.github.com>
2023-03-05 00:12:21 -0600

    Add files via upload

---
+++ _config.php
@@ -0,0 +1,29 @@
+<?php 
+
+$websiteTitle = "AniKatsu"; // Website Name
+$websiteUrl = "//{$_SERVER['SERVER_NAME']}";  // Website URL
+$websiteLogo = $websiteUrl . "/files/images/logo.png?v=2"; // Logo
+$contactEmail = "xyz@gmail.com"; // Contact Email
+
+$version = "0.3";
+
+//Donate 
+$donate = "#";
+
+// Socials 
+$telegram = "https://t.me/#"; // telegram
+$discord = "https://discord.com/#"; // Discord
+$redit = "#"; // Reddit
+$twitter = "#"; // Twitter
+ 
+
+
+$disqus = "https://http-www-wrizzlergang-xyz.disqus.com/"; // Disqus
+
+
+// API URL
+$api = "https://anikatsu-api.ovahpawer.repl.co/"; // https://github.com/shashankktiwariii/anikatsu-api
+
+
+$banner = $websiteUrl . "/files/images/banner.png";  //Banner
+?>
